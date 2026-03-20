bf41b89d13f7c8c21ef346f3110a1ac05c437f80
zjl2401 <3625547823@qq.com>
2026-03-20 10:01:59 +0800

    Update dataset_readers.py

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -284,7 +284,7 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
 
             norm_data = im_data / 255.0
             arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
-            image = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
+            image = Image.fromarray(np.array(arr*255.0, dtype=np.uint8), "RGB")
             image = PILtoTorch(image,(800,800))
             fovy = focal2fov(fov2focal(fovx, image.shape[1]), image.shape[2])
             FovY = fovy 
